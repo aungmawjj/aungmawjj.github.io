141956633ce1e1f863da78af38190e802cde7e44
Aung Maw <122535885+nug-dev-aung@users.noreply.github.com>
2024-01-18 21:01:33 +0800

    Improve responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,48 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.namecard}>
+        <div className={styles.portraitBox}>
+          <Image
+            src={"/portrait.jpeg"}
+            alt="My Portrait"
+            className={styles.portrait}
+            fill
+            priority
+          />
+        </div>
+        <h2 className={styles.name}>Aung Maw</h2>
+        <h3 className={styles.title}>Software Engineer</h3>
+        <Contact />
+      </div>
+
+      <div className={styles.about}>
+        <h1 className={styles.hello}>Hello!</h1>
+        <p>
+          Crafting code with passioin, I&apos;m a full-stack software engineer
+          dedicated to mastering technologies.
+        </p>
+        <p>
+          At DBS Bank, I orchestrated the deployment of Apache Spark Clusters,
+          designed REST APIs with FastAPI and Spring Boot, and optimized
+          performance through innovative solutions.
+        </p>
+        <p>
+          My journey also includes blockchain development at SUTD, earning
+          awards in national and international challenges.
+        </p>
+      </div>
+
+      <div className={styles.bottomBar}>
+        <Contact />
+      </div>
+    </main>
+  );
+}
+
 function Contact() {
   return (
     <div className={styles.contact}>
@@ -31,46 +73,5 @@ function Contact() {
         />
       </a>
     </div>
-  );
-}
-
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.namecard}>
-        <Image
-          src={"/portrait.jpeg"}
-          alt="My Portrait"
-          width={120}
-          height={120}
-          className={styles.portrait}
-          priority
-        />
-        <h2 className={styles.name}>Aung Maw</h2>
-        <h3 className={styles.title}>Software Engineer</h3>
-        <Contact />
-      </div>
-
-      <div className={styles.about}>
-        <h1 className={styles.hello}>Hello!</h1>
-        <p>
-          Crafting code with passioin, I&apos;m a full-stack software engineer
-          dedicated to mastering technologies.
-        </p>
-        <p>
-          At DBS Bank, I orchestrated the deployment of Apache Spark Clusters,
-          designed REST APIs with FastAPI and Spring Boot, and optimized
-          performance through innovative solutions.
-        </p>
-        <p>
-          My journey also includes blockchain development at SUTD, earning
-          accolades in national and international challenges.
-        </p>
-      </div>
-
-      <div className={styles.bottomBar}>
-        <Contact />
-      </div>
-    </main>
   );
 }
