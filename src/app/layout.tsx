01146f9ce0c74761cbcf1f7d44b5f97ab58b8af1
Aung Maw <aungmawjj12@gmail.com>
2024-07-16 23:32:04 +0800

    Update content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,7 @@ export const metadata: Metadata = {
   description: "Personal Website",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <div className={styles.about}>
         <div className={styles.section}>
           <h1 className={styles.hello}>Hello!</h1>
-          <p>Crafting code with passion, I&apos;m a software engineer dedicated to mastering technologies.</p>
+          <p>Crafting code with passion, I am a software engineer dedicated to mastering technologies.</p>
           <p>
             At DBS Bank, I orchestrated the deployment of Apache Spark Clusters, designed REST APIs with FastAPI and
             Spring Boot, and optimized performance through innovative solutions.
@@ -30,17 +30,43 @@ export default function Home() {
         <div className={styles.section}>
           <h2>Professional Enhancement</h2>
           <p>
-            Continuing to evolve in my career, I actively pursue opportunities to deepen my expertise. I&apos;ve earned
-            the following certifications:
+            Continuing to evolve in my career, I actively pursue opportunities to deepen my expertise. I have earned the
+            following certifications:
           </p>
-          <ul>
-            <li>
-              <p><b>CKAD</b>: Certified Kubernetes Application Developer</p>
-            </li>
-            <li>
-              <p><b>AWS</b> Certified Developer - Associate</p>
-            </li>
-          </ul>
+          <div className={styles.certificate}>
+            <a
+              href="https://www.credly.com/badges/1ba39b20-f663-41b9-bef3-74e6a49e963a/public_url"
+              target="_block"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src={"/ckad-certified-kubernetes-application-developer.png"}
+                alt="CKAD Badge"
+                priority={false}
+                className={styles.badge}
+                width={56}
+                height={56}
+              />
+            </a>
+            <span>CKAD: Certified Kubernetes Application Developer</span>
+          </div>
+          <div className={styles.certificate}>
+            <a
+              href="https://www.credly.com/badges/cac6cb50-e31b-486f-a108-ecb9f997e06c/public_url"
+              target="_block"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src={"/aws-certified-developer-associate.png"}
+                alt="AWS Badge"
+                priority={false}
+                className={styles.badge}
+                width={56}
+                height={56}
+              />
+            </a>
+            <span>AWS Certified Developer - Associate</span>
+          </div>
         </div>
 
         <div className={styles.section}>
@@ -63,7 +89,7 @@ export default function Home() {
 
         <div className={styles.section}>
           <h2>Projects</h2>
-          <p>I&apos;ve worked on a number of projects. Let me describe some here!</p>
+          <p>I have worked on a number of projects. Let me describe some here!</p>
           <ul>
             <li>
               <p className={styles.projectDesc}>
@@ -107,6 +133,29 @@ export default function Home() {
         </div>
 
         <div className={styles.section}>
+          <h2>Publications</h2>
+          <p>I have contributed to several publications in my field. Here is one highlight!</p>
+          <ul>
+            <li>
+              <p>ICS-BlockOpS: Blockchain for operational data security in industrial control system</p>
+              <p>Pervasive and Mobile Computing | Oct 2019</p>
+            </li>
+          </ul>
+          <p>
+            Please visit my{" "}
+            <a
+              href="https://scholar.google.com/citations?user=ELCqucwAAAAJ&hl=en"
+              target="_block"
+              rel="noopener noreferrer"
+              className={styles.textLink}
+            >
+              Google Scholar Profile
+            </a>{" "}
+            for a complete list of my publications,
+          </p>
+        </div>
+
+        <div className={styles.section}>
           <h2>Tech Stack</h2>
           <ul>
             <li>
@@ -132,6 +181,11 @@ export default function Home() {
             <li>
               <p>
                 <b>Observability</b>: Grafana, Logstash, Kibana
+              </p>
+            </li>
+            <li>
+              <p>
+                <b>Blockchain</b>: Solidity, Go Ethereum, Hyperledger Fabric
               </p>
             </li>
           </ul>
